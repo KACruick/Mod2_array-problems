9f809f886cb9ceb1350540b1991f781eb78adbbb
danehoov <DaneHoov22@gmail.com>
2024-05-11 12:25:44 -0600

    shuffle solved

--- array-practice.js
+++ array-practice.js
@@ -50,74 +50,61 @@ const smallerThanCurr = (arr) => {
   // time: O(n^2) and space: O(n)
 };
 
-
-
 const twoSum = (arr, target) => {
-  for(let i = 0; i < arr.length; i++){
-    let num1 = arr[i]
-    for(let j = i + 1; j< arr.length; j++){
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
+    for (let j = i + 1; j < arr.length; j++) {
       let num2 = arr[j];
-      if(num1 + num2 === target){
+      if (num1 + num2 === target) {
         return true;
       }
     }
   }
   return false;
+  // time: O(n^2) space: O(1)
 };
 
 const secondLargest = (arr) => {
-  // let max = arr[0];
-  // let secondMax = -Infinity;
-  // if(arr.length < 2) return undefined
-  // if (arr.length === 2){
-  //   if(arr[0] < arr[1]){
-  //     return arr[0]
-  //   } return arr[1]
-  // }
+  let max = arr[0];
+  debugger;
+  let secondMax = -Infinity;
+  if (arr.length < 2) return undefined;
+  if (arr.length === 2) {
+    if (arr[0] < arr[1]) {
+      return arr[0];
+    }
+    return arr[1];
+  }
 
-  // for (let i = 1; i < arr.length; i++){
-  //   if (arr[i] > max){
-  //     //secondMax = max
-  //     max = arr[i]
-  //   } else if(arr[i] < max && arr[i] > secondMax) {
-  //     secondMax = arr[i]
-  //   }
-  // }
-  // return secondMax
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      secondMax = max;
+      max = arr[i];
+    } else if (arr[i] < max && arr[i] > secondMax) {
+      secondMax = arr[i];
+    }
+  }
+  return secondMax;
 };
-
-// const arr = [4, 2, 3, 6, 8];
-// console.log(secondLargest(arr)); // => 6
-
-const arr2 = [4, 3, 2, 1];
-console.log(secondLargest(arr2)) // 3
-
-
-
-// let max = arr[0]
-// if(arr.length < 2) return undefined
-
-// for (let i = 0; i < arr.length; i++) {
-//   if (arr[i] > max){
-//     max = arr[i]
-//   }
-//   //arr.splice(max, 1)
-// }
-// console.log("max: ", max)
-// let secondMax = arr[0]
-// for (let i = 0; i < arr.length; i++) {
-//   if (secondMax < arr[i] && arr[i] !== max){
-//     secondMax = arr[i]
-//   }
-  
-// }
-// console.log("secondMax: ", secondMax)
-// return secondMax;
-
-
-
+//time: O(n) space: O(1)
+const getRandomInt = (max) => {
+  return Math.floor(Math.random()) * max;
+};
 const shuffle = (arr) => {
-  // Your code here
+  let copy = arr.slice();
+
+  for (let i = copy.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = copy[i];
+    copy[i] = copy[j];
+    copy[j] = temp;
+  }
+  return copy;
+  // let random = []
+
+  // let num = arr[getRandomInt(arr.length)]
+  // random.push(num)
+  // arr.splice(num, 1)
 };
 
 module.exports = [
