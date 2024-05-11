4cf51d99ab34945c9955a6b754244d1e504928fd
danehoov <DaneHoov22@gmail.com>
2024-05-11 11:03:45 -0600

    evenNumOfChars/smallerThanCurr

--- array-practice.js
+++ array-practice.js
@@ -1,55 +1,75 @@
-const findMinimum = arr => {
+const findMinimum = (arr) => {
   let min = arr[0];
-  for(let i = 1; i < arr.length; i++){ 
-    if (arr[i] < min){
-      min = arr[i]
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
     }
-    
   }
   return min;
   // time: O(n) and Space O(1)
 };
 
-
-const runningSum = arr => {
+const runningSum = (arr) => {
   let summedArray = [];
-  for(let i = 0; i < arr.length; i++){
-    if(i === 0) {
-      summedArray.push(arr[i])
-    }
-    else summedArray.push(arr[i] + summedArray[i-1])
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0) {
+      summedArray.push(arr[i]);
+    } else summedArray.push(arr[i] + summedArray[i - 1]);
   }
   return summedArray;
   // time: O(n) space: O(n)
 };
 
+const evenNumOfChars = (arr) => {
+  let count = 0;
 
-
-const evenNumOfChars = arr => {
-
-  // Your code here 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length % 2 === 0) {
+      count++;
+    }
+  }
+  return count;
+  //time: O(n) space: O(1)
 };
 
-const smallerThanCurr = arr => {
-
-  // Your code here 
-
+const smallerThanCurr = (arr) => {
+  let newArr = [];
+  // debugger;
+  for (let i = 0; i < arr.length; i++) {
+    let num1 = arr[i];
+    let count = 0;
+    for (let j = 0; j < arr.length; j++) {
+      let num2 = arr[j];
+      if (num2 < num1 && j !== i) {
+        count++;
+      }
+    }
+    newArr.push(count);
+  }
+  return newArr;
 };
+
+const arr = [8, 1, 2, 2, 3];
+smallerThanCurr(arr); // => [4,0,1,1,3]
 
 const twoSum = (arr, target) => {
-
-  // Your code here 
+  // Your code here
 };
 
-const secondLargest = arr => {
-
-  // Your code here 
+const secondLargest = (arr) => {
+  // Your code here
 };
 
 const shuffle = (arr) => {
-
-  // Your code here 
+  // Your code here
 };
 
-
-module.exports = [findMinimum, runningSum, evenNumOfChars, smallerThanCurr, twoSum, secondLargest, shuffle];
+module.exports = [
+  findMinimum,
+  runningSum,
+  evenNumOfChars,
+  smallerThanCurr,
+  twoSum,
+  secondLargest,
+  shuffle,
+];
